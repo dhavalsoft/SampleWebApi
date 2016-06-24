a3a0c92e458fbf9a46e85b22ab5106227b599a00
dhavalcric <dhaval.cric@gmail.com>
2016-06-24 08:03:57 -0400

    new solution added

---
+++ WebApiSample/NotFoundTextPlainActionResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApplication1
+{
+    public class NotFoundTextPlainActionResult : IHttpActionResult
+    {
+        Task<HttpResponseMessage> IHttpActionResult.ExecuteAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
